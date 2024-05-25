4d9e1caefdf7e83acb94a7a1157d61b39f7f0f79
justSmK <28645140+justSmK@users.noreply.github.com>
2024-05-25 23:20:28 +0300

    Project init with SPM Library

---
+++ ToNonRetard/ToNonRetardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToNonRetardApp.swift
+//  ToNonRetard
+//
+//  Created by Sergei Semko on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ToNonRetardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
